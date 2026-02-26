df713558ae910100d5f8f77aac3dd6f34cee11a6
Mason Jeffrey Dubelbeis <Mason.Dubelbeis@gmail.com>
2026-02-26 12:22:14 -0600

    feat: add basic user and product CRUD endpoints

---
+++ src/main/java/com/masondubelbeis/order_management_api/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.masondubelbeis.order_management_api.controller;
+
+import com.masondubelbeis.order_management_api.domain.Product;
+import com.masondubelbeis.order_management_api.repository.ProductRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
---
+++ src/main/java/com/masondubelbeis/order_management_api/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.masondubelbeis.order_management_api.controller;
+
+import com.masondubelbeis.order_management_api.domain.User;
+import com.masondubelbeis.order_management_api.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
